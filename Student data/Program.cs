ad3ef4f9b5401ff9256b77c8728237a7c550f6e8
Rasul1729 <rasulkader1729@outlook.com>
2023-10-23 16:06:28 +0530

    Add project files.

---
+++ Student data/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                try
+                {
+                    string path = @"C:\Users\new\source\repos\Student data\Student.txt";
+                    string[] lines = File.ReadAllLines(path);
+                    Console.WriteLine("The Students Data are \n");
+                    int index = 1;
+                    foreach (string line in lines)
+                        Console.WriteLine($" {index++} : " + line);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("All the Students data in the files are Displayed.");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
